3e65d0e8f660de0b94cf3adb1482413faddc75f9
Daniel Savage <dansvg@gmail.com>
2013-10-11 14:15:34 -0700

    Structured code a little more and added quotes up to 200

--- main.c
+++ main.c
@@ -6,19 +6,42 @@
 
 char* message = "Test message...";
 
+enum MessagePrintState
+{
+	INIT,
+	GIBBERISH,
+	QUOTE,
+	ART,
+};
+
+void printQuote()
+{
+	int i;
+	for (i = 0; i < strlen(message); i++)
+	{
+		printf("%c", message[i]);
+		fflush(stdout);
+		usleep(100 * 1000);
+	}
+
+	printf("\n");
+}
+
+void printGibberish()
+{
+	//
+}
+
+void printAsciiArt()
+{
+	//
+}
+
 int main (int argc, char* argv[])
 {
 	while(1)
 	{
-		int i;
-		for (i = 0; i < strlen(message); i++)
-		{
-			printf("%c", message[i]);
-			fflush(stdout);
-			usleep(100 * 1000);
-		}
-
-		printf("\n");
+		//
 	}
 
 	return 0;
