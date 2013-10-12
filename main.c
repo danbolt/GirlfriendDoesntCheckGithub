9da34f1d5ba791448f24a0e66f2fd75c67638d5e
Daniel Savage <dansvg@gmail.com>
2013-10-11 19:59:18 -0700

    randomly spits quotes

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+#define MAX_STRING_LENGTH 1000
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,16 +8,48 @@
 
 char* message = "Test message...";
 
-enum MessagePrintState
+typedef enum
 {
 	INIT,
 	GIBBERISH,
 	QUOTE,
 	ART,
-};
+} MessagePrintState;
+
+MessagePrintState state;
+
+char* quotes[201];
+
+void loadData(char* datafile)
+{
+	FILE* file;
+	file = fopen(datafile, "r");
+
+	char* buffer = calloc(250 * sizeof(char), 1);
+
+	int bufferCount = 0;
+
+	if (file != NULL)
+	{
+		printf("LOADING DATA...\n");
+
+		while (feof(file) == 0)
+		{
+			fgets(buffer, MAX_STRING_LENGTH, file);
+			quotes[bufferCount] = buffer;
+			buffer = calloc(250 * sizeof(char), 1);
+			bufferCount++;
+		}
+
+		fclose(file);
+	}
+}
 
 void printQuote()
 {
+	int randomQuote = rand() % 200;
+	char* message = quotes[randomQuote];
+
 	int i;
 	for (i = 0; i < strlen(message); i++)
 	{
@@ -39,9 +73,25 @@ void printAsciiArt()
 
 int main (int argc, char* argv[])
 {
+	state = INIT;
+
 	while(1)
 	{
-		//
+		switch (state)
+		{
+			case INIT:
+				loadData("data.txt");
+				printf("SUPER COOL TYPING PROGRAM\nSTARTUP!\n");
+				state = QUOTE;
+				break;
+			case GIBBERISH:
+				break;
+			case QUOTE:
+				printQuote();
+				break;
+			case ART:
+				break;
+		}
 	}
 
 	return 0;
